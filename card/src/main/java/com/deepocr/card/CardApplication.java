ce6fa3159d664de70153dfae703034f8f3e19790
HarryCheng <582416551@qq.com>
2018-12-11 13:43:25 +0800

    WEB端模版

---
+++ card/src/main/java/com/deepocr/card/CardApplication.java
@@ -0,0 +1,12 @@
+package com.deepocr.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardApplication.class, args);
+    }
+}
---
+++ card/src/main/java/com/deepocr/card/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.deepocr.card.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+}
