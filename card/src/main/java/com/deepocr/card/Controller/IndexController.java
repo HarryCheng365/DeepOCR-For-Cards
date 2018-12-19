e7684a24137e88a047bbfa52aa07bdb619316d57
HarryCheng <582416551@qq.com>
2018-12-19 10:31:07 +0800

    Summary

--- card/src/main/java/com/deepocr/card/Controller/IndexController.java
+++ card/src/main/java/com/deepocr/card/Controller/IndexController.java
@@ -1,13 +1,38 @@
 package com.deepocr.card.Controller;
 
+import com.deepocr.card.Service.FileUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-@RequestMapping("/task")
+@RequestMapping("/upload")
 public class IndexController {
+    @RequestMapping(value="/",method= RequestMethod.GET)
     public ModelAndView index(){
         return new ModelAndView("index");
     }
+
+    @RequestMapping(value="/image", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadImg(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+        try {
+            FileUtil.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "uploadimg success";
+    }
+
 }
