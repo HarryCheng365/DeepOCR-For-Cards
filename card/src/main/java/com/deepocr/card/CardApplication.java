9cfeff38593dcf6377906d1d72297f1ef785ec26
HarryCheng <582416551@qq.com>
2018-12-21 18:20:27 +0800

    实现了局域网拍照或者相册上传

--- card/src/main/java/com/deepocr/card/CardApplication.java
+++ card/src/main/java/com/deepocr/card/CardApplication.java
@@ -3,7 +3,11 @@ package com.deepocr.card;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @Configuration
@@ -11,5 +15,15 @@ public class CardApplication {
     @Autowired
     public static void main(String[] args) {
         SpringApplication.run(CardApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //  单个数据大小
+        factory.setMaxFileSize("10240KB");
+        /// 总上传数据大小
+        factory.setMaxRequestSize("10240KB");
+        return factory.createMultipartConfig();
     }
 }
